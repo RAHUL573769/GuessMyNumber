7023b06e4f79b85411fd18df149565e2013fdd72
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-15 19:27:33 +0600

    qwerty commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const a=document.querySelector('.message').textContent
+console.log(a);
+
+// document.getElementById.querySelector('score').textContent
+
+// console.log(document.getElementById.querySelector('score'));
+
+let b=document.querySelector('.score');
+
+b.textContent='13';
+
+document.querySelector('.number').innerHTML='13';
+document.querySelector('.guess').value='23';
+
+console.log(document.querySelector('.number').innerHTML);
