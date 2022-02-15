7d83036d0a59570a946a2d750a2c627ea4aec731
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-15 19:44:27 +0600

    qwervvvty commit

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 'use strict';
 
-const a=document.querySelector('.message').textContent
-console.log(a);
+// const a=document.querySelector('.message').textContent
+// console.log(a);
 
 // document.getElementById.querySelector('score').textContent
 
 // console.log(document.getElementById.querySelector('score'));
 
-let b=document.querySelector('.score');
+// let b=document.querySelector('.score');
 
-b.textContent='13';
+// b.textContent='13';
 
-document.querySelector('.number').innerHTML='13';
-document.querySelector('.guess').value='23';
+// document.querySelector('.number').innerHTML='13';
+// document.querySelector('.guess').value='23';
 
-console.log(document.querySelector('.number').innerHTML);
+// console.log(document.querySelector('.number').innerHTML);
+
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess=parseInt(document.querySelector('.guess').value);
+    console.log(typeof(guess));
+
+    if(!guess){
+        document.querySelector('.message').textContent="No Number";
+    }
+})
