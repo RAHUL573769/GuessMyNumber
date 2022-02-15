3cfb7fcd82cf262c6e0f4221c35ab86687ddd7b7
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-15 20:47:43 +0600

    qwerdddddty commit

--- script.js
+++ script.js
@@ -16,12 +16,49 @@
 
 // console.log(document.querySelector('.number').innerHTML);
 
-
+const secretNumber=parseInt(Math.trunc(Math.random()*20));
+// console.log(typeof(secretNumber));
+document.querySelector(".number").innerHTML=secretNumber;
+let score=20;
 document.querySelector('.check').addEventListener('click',function(){
     const guess=parseInt(document.querySelector('.guess').value);
-    console.log(typeof(guess));
+    // console.log((guess));
 
     if(!guess){
         document.querySelector('.message').textContent="No Number";
+    }
+    else if(guess===secretNumber){
+        document.querySelector('.message').textContent="You Have Won The Game";
+        
+    }
+    else if(guess>secretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent="Higher Guess";
+            score--;
+            document.querySelector('.score').innerHTML=score;
+        }
+        else{
+            score--;
+            document.querySelector('.message').innerHTML="You have Lost"; 
+        }
+        
+    }
+    else if(guess<secretNumber){
+
+        if(score>1){
+            document.querySelector('.message').textContent="Lower Guess";
+            score--;
+            document.querySelector('.score').innerHTML=score;
+        }
+        else{
+            score--;
+            document.querySelector('.message').innerHTML="You have Lost"; 
+        }
+
+
+
+
+
+      
     }
 })
